0a3bc19d154cddf28a9c63bdea4fef170b5059b4
egor_sorokin <sorokine329@gmail.com>
2025-05-11 02:00:43 +0300

    create insertTestQuestionsAnswers method, class Test, class Question, class Answer for parsing

---
+++ src/main/java/telegrambot/model/Genre.java
@@ -0,0 +1,22 @@
+package telegrambot.model;
+
+public class Genre {
+    private String code;
+    private String name;
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
